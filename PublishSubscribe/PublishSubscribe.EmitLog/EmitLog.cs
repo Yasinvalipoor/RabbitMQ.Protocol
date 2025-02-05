4993fbfc98190392d00b598a517e0db728d106cb
Yasinvalipoor <yasinvalipoor14@gmail.com>
2025-02-05 14:38:24 +0330

    Publish/Subscribe Completed

--- PublishSubscribe/PublishSubscribe.EmitLog/EmitLog.cs
+++ PublishSubscribe/PublishSubscribe.EmitLog/EmitLog.cs
@@ -1,3 +1,23 @@
-﻿using RabbitMQ.Protocol.Common;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Protocol.Common;
 
 using var channel = await Producer.CreateConnectedChannelFactory("localhost");
+
+
+await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+
+
+for (int i = 1; i <= 50; i++)
+{
+    string message = $"Task {i}";
+    await Producer.ProducerPayloadAsync(channel, "logs", string.Empty, message);
+    // Use BasicPublishAsync() Seconde Overload - Give one properties for {Persistent = true}
+    // {Persistent = true} => RR - Messages are not lost if RabbitMQ is restarted.
+
+    Console.WriteLine($" [x] Sent {message}");
+    await Task.Delay(1000);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
