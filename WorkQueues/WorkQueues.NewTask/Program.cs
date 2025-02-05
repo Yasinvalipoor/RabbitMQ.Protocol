d389532bdda2bffdcb79445038eca7c7acb80bc2
Yasinvalipoor <yasinvalipoor14@gmail.com>
2025-02-05 11:11:15 +0330

    completed WorkQueues

--- WorkQueues/WorkQueues.NewTask/Program.cs
+++ WorkQueues/WorkQueues.NewTask/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Protocol.Common;
+
+using var channel = await Producer.CreateConnectedChannelFactory("localhost");
+
+string queueName = "WorkQueues";
+
+
+await Producer.CreateQueueFromChannel(channel, queueName);
+
+var properties = new BasicProperties
+{
+    Persistent = true
+};
+
+for (int i = 1; i <= 50; i++)
+{
+    string message = $"Task {i}";
+    await Producer.ProducerPayloadAsync(channel, queueName, message, properties);
+    // Use BasicPublishAsync() Seconde Overload - Give one properties for {Persistent = true}
+    // {Persistent = true} => RR - Messages are not lost if RabbitMQ is restarted.
+
+    Console.WriteLine($" [x] Sent {message}");
+    await Task.Delay(1000);
+}
