55b185327136c309253922fff19b857338107e1f
Yasinvalipoor <yasinvalipoor14@gmail.com>
2025-02-08 12:21:27 +0330

    Protocol Topics - Completed

---
+++ Topics/Topics.EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Protocol.Common;
+
+
+using var channel = await Producer.CreateConnectedChannelFactory("localhost");
+
+await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
+
+
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+
+await Producer.ProducerPayloadAsync(channel, exchangeName: "topic_logs", routingKey, message);
+
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
---
+++ Topics/Topics.ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Protocol.Common;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+using var channel = await Consumer.CreateConnectedChannelFactory("localhost");
+
+await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
+
+// declare a server-named queue
+QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+string queueName = queueDeclareResult.QueueName;
+
+foreach (string? bindingKey in args)
+{
+    await channel.QueueBindAsync(queue: queueName, exchange: "topic_logs", routingKey: bindingKey);
+}
+
+Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+await Consumer.ConsumePayloadAsync(channel, queueName);
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
